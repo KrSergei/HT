49faabf838d204f80202d48817e24f5487e86162
KrSergei <34679066+KrSergei@users.noreply.github.com>
2024-07-21 18:09:20 +0300

    1

--- Threads/Threads/obj/Debug/net8.0/Threads.AssemblyInfo.cs
+++ Threads/Threads/obj/Debug/net8.0/Threads.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Threads")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48014bf4a84e0750c9213000bd17b41b4ce47e65")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+afb109045e64bc01a1ef1727191611587c02b4cf")]
 [assembly: System.Reflection.AssemblyProductAttribute("Threads")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Threads")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
