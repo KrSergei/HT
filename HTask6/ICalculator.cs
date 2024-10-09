15a980b503e8f2d451557c80afc0a170fee1a1cd
KrSergei <34679066+KrSergei@users.noreply.github.com>
2024-10-09 19:31:56 +0300

    1

---
+++ HTask6/ICalculator.cs
@@ -0,0 +1,14 @@
+﻿
+namespace HTask6
+{
+    internal interface ICalculator
+    {
+        void Sum(int value);
+
+        void Substract(int value);
+        void Myltiply(int value);
+        void Divide(int value);
+
+        void CancelLast();
+    }
+}
---
+++ HTask6/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HTask6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
